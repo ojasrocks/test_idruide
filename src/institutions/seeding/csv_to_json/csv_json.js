3381ec6a3ff883b3e33310cf3bb2b33a2b894f0f
Ojas <91060706+ojasrocks@users.noreply.github.com>
2022-01-29 21:50:45 +0100

    Add files via upload

--- src/institutions/seeding/csv_to_json/csv_json.js
+++ src/institutions/seeding/csv_to_json/csv_json.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const MongoClient = require('mongodb').MongoClient;
 const csvjson = require('csvjson');
+const axios = require('axios');
+
 
 const mongo_url = 'mongodb://127.0.0.1:27017';
 
@@ -14,37 +16,37 @@ var opt = {
 };
 
 const client = new MongoClient(mongo_url);
+(async()=> {
+  const csv_origin = await axios.get('https://data.education.gouv.fr/explore/dataset/fr-en-annuaire-education/download/?format=csv&timezone=Europe/Berlin&lang=fr&use_labels_for_header=true&csv_separator=%3B');
 
-const csv_origin = {
-  value : fs.readFileSync('./src/institutions/seeding/csv_to_json/fr-en-annuaire-education.csv', 'utf8')
-}
-
-const json_obj = csvjson.toObject(csv_origin.value, opt);
-
-const geo_json_obj = json_obj.map(item => {
-  let buff = item;
-  if (item.position){
-    buff.position = {
-      type : "Point",
-      coordinates : item.position.split(',').map(coord => parseFloat(coord))
-   }
-  } else {
-    buff.position = {
-      type : "Point",
-      coordinates : [0.0000000,0.0000000]
+  const json_obj = csvjson.toObject(csv_origin.data, opt);
+  
+  const geo_json_obj = json_obj.map(item => {
+    let buff = item;
+    if (item.position){
+      buff.position = {
+        type : "Point",
+        coordinates : item.position.split(',').map(coord => parseFloat(coord))
+     }
+    } else {
+      buff.position = {
+        type : "Point",
+        coordinates : [0.0000000,0.0000000]
+      }
     }
-  }
-  return buff;
-})
+    return buff;
+  })
+  
+  client.connect(async (err)=>{
+    if (!err){
+      console.log("Connected successfully to database server");
+      const collection = client.db(db_name).collection(collection_name);
+      await collection.insertMany(geo_json_obj)
+      await collection.createIndex({ position : '2dsphere' })
+      console.log('Database initialized');
+      process.exit();
+    }else{console.log(err);}
+  })
+})()
 
-client.connect(async (err)=>{
-  if (!err){
-    console.log("Connected successfully to database server");
-    const collection = client.db(db_name).collection(collection_name);
-    await collection.insertMany(geo_json_obj)
-    await collection.createIndex({ position : '2dsphere' })
-    console.log('Database initialized');
-    process.exit();
-  }else{console.log(err);}
-})
 
